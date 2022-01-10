2968d6c2c00616311dbeb20d28ac7b326098a4f2
Neeraj <neerajbabu27@gmail.com>
2022-01-10 17:43:19 +0530

    fixed ejs rendering

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const res = require('express/lib/response');
 const nodemon = require('nodemon');
+const morgan = require("morgan")
 
 
 const app = express();
+
+
+app.set('view engine', 'ejs');
+
 
 const port = 3000;
 app.listen(port);
@@ -11,11 +16,18 @@ app.listen(port);
 
 console.log('server running at ' + port);
 
+
+
+app.use(morgan('dev'));
+
+
+
+
 const root_path = __dirname
 // home page
 
 app.get('/', (req, res) => {
-    res.sendFile("./view/index.html", {root:root_path});
+    res.render('index');
 });
 
 
@@ -23,19 +35,29 @@ app.get('/', (req, res) => {
 // edit page
 
 app.get('/edit', (req, res) => {
-    res.send('<h1>edit</h1>');
+    res.render('editor');
 });
 
 
 // blog page
 
 app.get('/blog', (req, res) => {
-    res.send('<h1>blog</h1>');
+    res.render('blog');
 });
 
 // create page
 
 app.get('/create', (req, res) => {
-    res.send('<h1>blog</h1>');
+    res.render('create');
 });
 
+// about page
+
+app.get('/create', (req, res) => {
+    res.sendFile('./views/about.html',{root:root_path});
+});
+//404 page
+
+app.use((req, res) => {
+    res.sendFile('./views/404.html',{root:root_path});
+});
