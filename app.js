ddab36699a025d274829ce29aed59c72ed384265
Neeraj <neerajbabu27@gmail.com>
2022-01-10 18:18:23 +0530

    fixed routings

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const root_path = __dirname
 // home page
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index' , { title:'Home'});
 });
 
 
@@ -35,29 +35,29 @@ app.get('/', (req, res) => {
 // edit page
 
 app.get('/edit', (req, res) => {
-    res.render('editor');
+    res.render('editor',{ title:'Editor'});
 });
 
 
 // blog page
 
 app.get('/blog', (req, res) => {
-    res.render('blog');
+    res.render('blog',{ title:'Blog'});
 });
 
 // create page
 
 app.get('/create', (req, res) => {
-    res.render('create');
+    res.render('create',{ title:'Create'});
 });
 
 // about page
 
-app.get('/create', (req, res) => {
-    res.sendFile('./views/about.html',{root:root_path});
+app.get('/about', (req, res) => {
+    res.render('about',{ title:'About'});
 });
 //404 page
 
 app.use((req, res) => {
-    res.sendFile('./views/404.html',{root:root_path});
+    res.render('404',{ title:'Oops!'});
 });
